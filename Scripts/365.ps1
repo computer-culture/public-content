afa368288947730b5f9d4d30f143312310a3160d
hughburns-ccl <47095810+hughburns-ccl@users.noreply.github.com>
2024-07-26 11:43:55 +1200

    Update 365.ps1

--- Scripts/365.ps1
+++ Scripts/365.ps1
@@ -1125,7 +1125,7 @@ function Initialize-Environment
         $TenantPrimaryDomain        = $M365TenantPrimaryDomain
     }
 
-    Write-Host "App ID: $($global:AzureAdAppId)"
+    "App ID: $($global:AzureAdAppId)"
 
     # Create local storage folder if it doesn't already exist
     if (!(Test-Path $LocalDataSaveLocation))
